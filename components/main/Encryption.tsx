5865fd509e7070064c75ea4cbe992d7e3928b422
Fabricejor <Fabricejordan2001@gmail.com>
2024-08-29 10:52:37 +0000

    step 2  ajustement des diplômes

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -7,6 +7,11 @@ import { slideInFromLeft, slideInFromTop ,slideInFromRight } from '@/utils/motio
 
 
 const Encryption = () => {
+  const handleClick1 = () => {
+    // Rediriger vers une autre page
+    window.location.href = 'https://gomycode.com/';
+  };
+
   return (
     <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
       <div className="absolute w-auto h-auto top-0 z-[5]">
@@ -28,29 +33,32 @@ const Encryption = () => {
       <motion.div
     initial='hidden'
     animate='visible'
-    className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20] max-sm:flex-col max-sm:mb-[10%] max-sm:mt-[200px]'>
-      <div className='h-full w-full flex flex-col gap-5 justify center m-auto text-start'>
+    className='flex flex-row items-center justify-center px-20 mt-0 w-full z-[20] max-sm:flex-col max-sm:mb-[10%] max-sm:mt-[200px]'>
+      <div className='h-full w-full flex flex-col justify center m-auto text-start'>
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-2xl max-sm:text-1xl font-bold text-white max-w-[400px] w-auto "
+          className="flex flex-col  mt-6  font-bold text-white max-w-[500px] w-auto"
         >
-          <span className="TitredelaFormation">
+          <span className="TitredelaFormation  text-2xl">
           Bootcamp Fullstack Js, Gomycode, Dakar
+          </span>
+          <span className="text-gray-400 text-1x1 italic">
+            Novembre 2023 - Mai 2024
           </span>
         </motion.div>
         <motion.p
           variants={slideInFromLeft(0.8)}
-          className="text-1x1 text-gray-400 my-5 max-w-[400px] text-justify"
+          className="text-1x1 text-gray-200 my-5 max-w-[500px] text-justify"
         >
          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-          href='#skills'
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[100px]"
+          href='https://diploma.gomycode.app/?id=31716817358394535'
+          target="_Blank"
         >
-         {/* <Link href="#skills"> Voir plus!</Link> */}
-         <span  title='mes skills'>voir plus</span>
+         <span  title='mes skills' onClick={handleClick1}>voir plus</span>
         </motion.a>
       </div>
       <motion.div
@@ -58,12 +66,10 @@ const Encryption = () => {
         className="w-full h-full flex justify-center items-center  max-sm:mt-8  max-sm:w-[150px]"
       >
         <Image
-          src={'/PhotoProfil.png'}
+          src={'/gomycode.jpg'}
           alt="profile"
-          height={350}
-          width={350}
-          // onMouseEnter={() => setImageSrc('/PhotoProfil.png')}
-          // onMouseLeave={() => setImageSrc('/PhotoProfil.png')}
+          height={150}
+          width={150}
           className='rounded-[40%] filter grayscale opacity-80 hover:grayscale-0 hover:opacity-100 transition-all duration-1000'
         />
       </motion.div>
@@ -72,47 +78,44 @@ const Encryption = () => {
     <motion.div
     initial='hidden'
     animate='visible'
-    className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20] max-sm:flex-col max-sm:mb-[10%] max-sm:mt-[200px]'>
-      <div className='h-full w-full flex flex-col gap-5 justify center m-auto text-start'>
+    className='flex flex-row items-center justify-center px-20 mt-10 w-full z-[20] max-sm:flex-col max-sm:mb-[10%] max-sm:mt-[200px]'>
+      <div className='h-full w-full flex flex-col justify center m-auto text-start'>
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl max-sm:text-4xl font-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col  mt-6  font-bold text-white max-w-[550px] w-auto"
         >
-          <span>
-            Dévelloppeur
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              {" "}
-              Fullstack Js
-              {" "}
-            </span>
+          <span className="TitredelaFormation  text-2xl">
+          Licence Professionnelle en Génie Logicielle, École supérieur
+          Polytechnique, Dakar
+          </span>
+          <span className="text-gray-400 text-1x1 italic">
+          Decembre 2020 — Juillet 2023
           </span>
         </motion.div>
         <motion.p
           variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px] text-justify"
+          className="text-1x1 text-gray-200 my-5 max-w-[500px] text-justify"
         >
-          Développeur Full Stack passionné par le numérique et les nouvelles technologies, j'ai une solide expérience en développement web et mobile. Toujours à l'affût de nouveaux défis, je cherche sans cesse à perfectionner mes compétences techniques. Mon objectif est de contribuer activement à la réussite de vos projets.
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-          href='#skills'
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[100px]"
+          href='https://diploma.gomycode.app/?id=31716817358394535'
+          target="_Blank"
         >
-         {/* <Link href="#skills"> Voir plus!</Link> */}
-         <span  title='mes skills'>voir plus</span>
+         <span  title='mes skills' onClick={handleClick1}>voir plus</span>
         </motion.a>
       </div>
       <motion.div
         variants={slideInFromRight(0.8)}
-        className="w-full h-full flex justify-center items-center  max-sm:mt-8  max-sm:w-[250px]"
+        className="w-full h-full flex justify-center items-center  max-sm:mt-8  max-sm:w-[150px]"
       >
         <Image
-          src={'/PhotoProfil.png'}
+          src={'/gomycode.jpg'}
           alt="profile"
-          height={350}
-          width={350}
-          // onMouseEnter={() => setImageSrc('/PhotoProfil.png')}
-          // onMouseLeave={() => setImageSrc('/PhotoProfil.png')}
+          height={150}
+          width={150}
           className='rounded-[40%] filter grayscale opacity-80 hover:grayscale-0 hover:opacity-100 transition-all duration-1000'
         />
       </motion.div>
