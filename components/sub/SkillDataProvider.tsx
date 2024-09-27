a8e2ec007ce1761b6b5da4270447f8ab7ab1fc7e
Fabricejor <Fabricejordan2001@gmail.com>
2024-09-27 14:36:35 +0000

    ajout de meta donné +indexation

--- components/sub/SkillDataProvider.tsx
+++ components/sub/SkillDataProvider.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import {motion} from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import Image from 'next/image';
-import { title } from 'process';
+
 
 interface Props {
     src: string;
